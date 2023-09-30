a8e6e4ca83998cf3cec4b4b7c6e7e7a32ff449e2
Ray <ray_punkeki@hotmail.com>
2023-09-29 22:43:45 -0500

    feat: components and settings added

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+    Usuario_ID: any;
+    Nombres: any;
+    Apellidos: any;
+    correo: any;
+    numero_telefono: any;
+    Credencial_Credencial_ID: any;
+    Localidad_Localidad_ID: any;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+//Ruta local
+export const environment = {
+    development: false,
+    //baseURL:'http://20.25.184.87:5000'
+    baseURL:'http://127.0.0.1:5000'
+};
